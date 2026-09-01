eca138607114fec14923c3e91f1f2ce3619eb1ce
shwetarajpara <shwetarajpara07@gmail.com>
2026-09-01 17:20:30 +0530

    range enum change

--- models/defaultParameterRange.js
+++ models/defaultParameterRange.js
@@ -24,7 +24,7 @@ const defaultParameterRangeSchema = new mongoose.Schema({
 
     gender: {
         type: String,
-        enum: ['MALE', 'FEMALE', 'BOTH'],
+        enum: ['MALE', 'FEMALE', 'ANY'],
         required: true
     },
 
